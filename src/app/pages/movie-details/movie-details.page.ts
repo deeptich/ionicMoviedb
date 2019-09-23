ff09942fb9fa292949885054f1130c1325554ac3
Deepti Chauhan <deepti_c@rediffmail.com>
2019-09-23 16:36:13 +0530

    first commit

---
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -0,0 +1,68 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+import { MovieService } from './../../services/movie.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+ 
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.page.html',
+  styleUrls: ['./movie-details.page.scss'],
+})
+export class MovieDetailsPage implements OnInit {
+ 
+  information = null;
+ 
+  /**
+   * Constructor of our details page
+   * @param activatedRoute Information about the route we are on
+   * @param movieService The movie Service to get data
+   */
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
+ 
+  ngOnInit() {
+    // Get the ID that was passed with the URL
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+ 
+    // Get the information from the API
+    this.movieService.getDetails(id).subscribe(result => {
+      this.information = result;
+    });
+  }
+ 
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
+  }
+}
